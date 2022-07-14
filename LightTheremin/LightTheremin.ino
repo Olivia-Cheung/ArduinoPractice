c223b5dd0d44467e6dc0dd408ad487c23cdd538b
Olivia Cheung <84092184+OCheung313@users.noreply.github.com>
2022-07-14 16:09:30 -0500

    Added more descriptive comments

--- LightTheremin/LightTheremin.ino
+++ LightTheremin/LightTheremin.ino
@@ -8,22 +8,25 @@ const int ledPin = 13;
 void setup() {
   // put your setup code here, to run once:
 
+//creates ledPin into an outpu and turns it on
   pinMode(ledPin, OUTPUT); 
   digitalWrite(ledPin, HIGH);
 
+//calibrates the surrounding light for the first five seconds the program runs
   while(millis() < 5000){
     sensorValue = analogRead(A0); 
-
+  //finds the maximum sensor value
     if(sensorValue > sensorHigh){
       sensorHigh = sensorValue;
     }
+   //findt the minimum sensor value
     if(sensorValue < sensorLow){
 
       sensorLow = sensorValue; 
     }
     
   }
-
+//calibrate finishes and ledPin turns off
   digitalWrite(ledPin, LOW); 
 
 }
@@ -31,10 +34,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+//initializes sensorValue to A0 port
   sensorValue = analogRead(A0); 
-
+//maps the sensor value to a range of pitches
   int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000); 
 
+//plays the tone for 20 milliseconds on pin 8 and waits for a second
   tone(8, pitch, 20); 
   delay(10);
 
